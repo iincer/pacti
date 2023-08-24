4961c0d9c73f3b2dd15ce34eeba894a9da66da65
Inigo Incer <inigo@eecs.berkeley.edu>
2023-08-24 10:33:40 -0700

    Added capability to benchmark for varying densities

--- examples/benchmarking.py
+++ examples/benchmarking.py
@@ -8,23 +8,33 @@ import time
 import pickle
 
 
+
+
+import scipy.sparse as sp
+
+S = sp.random(3, 4, density=0.25)
+
+
+
 # number of variables
 n = 5
 # number of variables to elim
 m = 4
 # number of constraints in context
 N = 10
+# density
+density = 0.25
 
 
 def getRuntime(n,m,N):
     rtimes = []
     variables = [Var(f"v{i}") for i in range(n)]
-    for i in range(10000):
+    for i in range(50000):
         #print(i)
         a = np.random.random((1,n))
         b = np.random.random((1,1))
         term    = PolyhedralTermList.polytope_to_termlist(matrix=a,vector=b,variables=variables).terms[0]
-        a = np.random.random((N,n))
+        a = sp.random(N, n, density=0.25).A
         b = np.random.random((N,1))
         context = PolyhedralTermList.polytope_to_termlist(matrix=a,vector=b,variables=variables)
 
@@ -45,7 +55,7 @@ def getRuntime(n,m,N):
     return np.mean(a)
 
 
-m = 2
+m = 4
 
 dataArray = []
 
@@ -61,5 +71,6 @@ for n in [5, 10, 15, 20, 25, 30]:
 print(dataArray)
 
 
-with open('m2data.pickle', 'wb') as f:
+filename = 'm' + str(m)+ 'data'+'den'+str(density)+'.pickle'
+with open(filename, 'wb') as f:
     pickle.dump(dataArray, f)
